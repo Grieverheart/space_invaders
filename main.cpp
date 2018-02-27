45d5a5fe592741754a8ca8dc1b635ffb5f2d1d32
Grieverheart <nicktasios@gmail.com>
2018-02-27 11:42:02 +0100

    Keyboard input for player movement implemented.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+bool game_running = false;
+int move_dir = 0;
+
 #define GL_ERROR_CASE(glerror)\
     case glerror: snprintf(error, sizeof(error), "%s", #glerror)
 
@@ -56,6 +59,24 @@ bool validate_program(GLuint program){
 void error_callback(int error, const char* description)
 {
     fprintf(stderr, "Error: %s\n", description);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
+    switch(key){
+    case GLFW_KEY_ESCAPE:
+        if(action == GLFW_PRESS) game_running = false;
+        break;
+    case GLFW_KEY_RIGHT:
+        if(action == GLFW_PRESS) move_dir += 1;
+        else if(action == GLFW_RELEASE) move_dir -= 1;
+        break;
+    case GLFW_KEY_LEFT:
+        if(action == GLFW_PRESS) move_dir -= 1;
+        else if(action == GLFW_RELEASE) move_dir += 1;
+        break;
+    default:
+        break;
+    }
 }
 
 struct Buffer
@@ -167,6 +188,8 @@ int main(int argc, char* argv[])
         glfwTerminate();
         return -1;
     }
+
+    glfwSetKeyCallback(window, key_callback);
 
     glfwMakeContextCurrent(window);
 
@@ -371,8 +394,10 @@ int main(int argc, char* argv[])
 
     uint32_t clear_color = rgb_to_uint32(0, 128, 0);
 
-    int player_move_dir = 1;
-    while (!glfwWindowShouldClose(window))
+    game_running = true;
+
+    int player_move_dir = 0;
+    while (!glfwWindowShouldClose(window) && game_running)
     {
         buffer_clear(&buffer, clear_color);
 
@@ -409,17 +434,20 @@ int main(int argc, char* argv[])
 
         glfwSwapBuffers(window);
 
-        if(game.player.x + player_sprite.width + player_move_dir >= game.width - 1)
+        player_move_dir = 2 * move_dir;
+
+        if(player_move_dir != 0)
         {
-            game.player.x = game.width - player_sprite.width - player_move_dir - 1;
-            player_move_dir *= -1;
+            if(game.player.x + player_sprite.width + player_move_dir >= game.width)
+            {
+                game.player.x = game.width - player_sprite.width;
+            }
+            else if((int)game.player.x + player_move_dir <= 0)
+            {
+                game.player.x = 0;
+            }
+            else game.player.x += player_move_dir;
         }
-        else if((int)game.player.x + player_move_dir <= 0)
-        {
-            game.player.x = 0;
-            player_move_dir *= -1;
-        }
-        else game.player.x += player_move_dir;
 
         glfwPollEvents();
     }
